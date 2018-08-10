e9422f20fd56419c7616153824e189a4215bb879
Pierre Salaun <pierresalaun10@hotmail.com>
2018-08-10 11:45:41 +0200

    edit dose added. Prepare for heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,10 +5,10 @@ class DosesController < ApplicationController
   #   @doses = Dose.all
   # end
 
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
     @dose = Dose.new(set_params)
@@ -18,6 +18,27 @@ class DosesController < ApplicationController
     else
       render(:new)
     end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.find(params[:dose_id])
+    render 'cocktails/show'
+  end
+
+  def update
+    @dose = Dose.find(params[:id])
+    if @dose.update(set_params)
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render(:edit)
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [ :new, :create]
+    resources :doses, only: [ :create, :update]
   end
-  resources :doses, only: [:edit, :update, :destroy]
+  resources :doses, only: [ :update, :destroy]
+  get "cocktails/:id/doses/:dose_id", to: "doses#edit", as: :new_or_edit_dose
+  # get "cocktails/:id/doses/:dose_id", to: "doses#edit", as: :edit_dose
+  # post "cocktails/:id/doses", to: "doses#create"
 end
 
 # A user can see the list of cocktails
